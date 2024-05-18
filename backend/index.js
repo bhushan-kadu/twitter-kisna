a20a9f228bc9323eb2bf57dc1363ed5b00b01d8c
Krishna Alaspure <38836025+KrishnaAlaspure@users.noreply.github.com>
2024-05-18 11:45:28 +0530

    full code cors7

--- backend/index.js
+++ backend/index.js
@@ -30,11 +30,11 @@ dotenv.config({
     app.use(cookieParser())
     
 const corsOptions={
-    origin: "http://localhost:5173/",
+    origin: "*",
     credentials: true,
     optionSuccessStatus:200
 }
-app.use(cors())
+app.use(cors(corsOptions))
 
 
     //api
