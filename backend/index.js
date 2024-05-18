0601a967bce5478fcf09c394ccbdb76993720d1d
Krishna Alaspure <38836025+KrishnaAlaspure@users.noreply.github.com>
2024-05-18 11:34:26 +0530

    full code cors5

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ const corsOptions={
 app.use(cors(corsOptions))
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173/");
     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
