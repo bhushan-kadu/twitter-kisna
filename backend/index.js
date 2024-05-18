0915a3a7e2e8b7628669c34fc63dc7b94e553324
Krishna Alaspure <38836025+KrishnaAlaspure@users.noreply.github.com>
2024-05-18 16:25:54 +0530

    full code cors9

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,12 @@ const corsOptions={
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions))
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
 
 
     //api
