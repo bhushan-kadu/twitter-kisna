0d5ac207a4260ec3adb3c17ee9fa879c47bc4f5e
Krishna Alaspure <38836025+KrishnaAlaspure@users.noreply.github.com>
2024-05-18 17:43:21 +0530

    cors error 4

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE ,OPTIONS ");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
 
